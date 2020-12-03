46841784070ef50b42e4a81d407a1c63e08c2e27
Eric Marti <EricJMarti@users.noreply.github.com>
2020-12-02 19:54:00 -0500

    Adding ability to hunt for Newegg combo deals

--- src/scraper/newegg.py
+++ src/scraper/newegg.py
@@ -9,34 +9,66 @@ class NeweggScrapeResult(ScrapeResult):
         alert_content = ''
 
         # get name of product
+        is_combo = False
         tag = self.soup.body.find('h1', class_='product-title')
         if tag:
             alert_content += tag.text.strip() + '\n'
         else:
-            logging.warning(f'missing title: {self.url}')
-
-        buy_box = self.soup.body.find('div', class_='product-buy-box')
-        if buy_box:
-
-            # get listed price
-            tag = buy_box.find('li', class_='price-current')
-            price_str = self.set_price(tag)
-            if price_str:
-                alert_subject = f'In Stock for {price_str}'
-            else:
-                logging.warning(f'missing price: {self.url}')
-
-            # check for add to cart button
-            tag = buy_box.find('div', class_='product-buy')
+            # check if this is a combo
+            tag = self.soup.body.select_one('div.grpDesc > div.wrapper > h1')
             if tag:
-                if 'add to cart' in tag.text.lower():
-                    self.alert_subject = alert_subject
-                    self.alert_content = f'{alert_content.strip()}\n{self.url}'
+                alert_content += tag.text.strip() + '\n'
+                is_combo = True
             else:
-                logging.warning(f'missing add to cart button: {self.url}')
+                logging.warning(f'missing title: {self.url}')
+
+        if is_combo:
+            buy_box = self.soup.body.find('div', class_='grpPricing')
+            if buy_box:
+
+                # get listed price
+                tag = buy_box.select_one('div#singleFinalPrice.current')
+                price_str = self.set_price(tag.text.replace('Now:', ''))
+                if price_str:
+                    alert_subject = f'In Stock for {price_str}'
+                else:
+                    logging.warning(f'missing combo price: {self.url}')
+
+                # check for add to cart button
+                tag = buy_box.select_one('div.grpAction > a.atnPrimary')
+                if tag:
+                    if 'add to cart' in tag.text.lower():
+                        self.alert_subject = alert_subject
+                        self.alert_content = f'{alert_content.strip()}\n{self.url}'
+                else:
+                    logging.warning(f'missing combo add to cart button: {self.url}')
+
+            else:
+                logging.warning(f'missing combo buy box: {self.url}')
 
         else:
-            logging.warning(f'missing buy box: {self.url}')
+            buy_box = self.soup.body.find('div', class_='product-buy-box')
+            if buy_box:
+
+                # get listed price
+                tag = buy_box.find('li', class_='price-current')
+                price_str = self.set_price(tag)
+                if price_str:
+                    alert_subject = f'In Stock for {price_str}'
+                else:
+                    logging.warning(f'missing price: {self.url}')
+
+                # check for add to cart button
+                tag = buy_box.find('div', class_='product-buy')
+                if tag:
+                    if 'add to cart' in tag.text.lower():
+                        self.alert_subject = alert_subject
+                        self.alert_content = f'{alert_content.strip()}\n{self.url}'
+                else:
+                    logging.warning(f'missing add to cart button: {self.url}')
+
+            else:
+                logging.warning(f'missing buy box: {self.url}')
 
 
 @ScraperFactory.register
