44b90e2ab229b00403aa0967a4adee2e232ebc90
Eric Marti <EricJMarti@users.noreply.github.com>
2020-12-07 22:34:42 -0500

    Adding more Newegg unit tests

--- tests/newegg/test.py
+++ tests/newegg/test.py
@@ -16,7 +16,26 @@ def load_result(filename):
         return ScrapeResult(logging.getLogger(), response, None)
 
 
-class Fixture(unittest.TestCase):
+class BundleInStockFixture(unittest.TestCase):
+    def setUp(self):
+        self.result = load_result('bundle_in_stock.html')
+
+    def test_in_stock(self):
+        self.assertTrue(self.result)
+
+    def test_price(self):
+        self.assertEqual(self.result.price, 541.98)
+
+
+class BundleOutOfStockFixture(unittest.TestCase):
+    def setUp(self):
+        self.result = load_result('bundle_out_of_stock.html')
+
+    def test_in_stock(self):
+        self.assertFalse(self.result)
+
+
+class InStockFixture(unittest.TestCase):
     def setUp(self):
         self.result = load_result('in_stock.html')
 
@@ -25,3 +44,11 @@ class Fixture(unittest.TestCase):
 
     def test_price(self):
         self.assertEqual(self.result.price, 154.99)
+
+
+class OutOfStockFixture(unittest.TestCase):
+    def setUp(self):
+        self.result = load_result('out_of_stock.html')
+
+    def test_in_stock(self):
+        self.assertFalse(self.result)
