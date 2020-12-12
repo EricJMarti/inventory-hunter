9f3933084cc3c9e658f3dd8c28916fed5784b439
MRizkBV <Mohd.rizk21@outlook.com>
2020-12-13 00:41:23 +0300

    Update discord.py

--- src/alerter/discord.py
+++ src/alerter/discord.py
@@ -38,8 +38,8 @@ class DiscordAlerter(Alerter):
             "embeds": [
                 {"title": kwargs.get("subject"), "description": content, "color": 5832569}
             ],
-            "username": "StockRadar - ALERT",
-            "avatar_url": "https://i.imgur.com/RF6DrjG.png",
+            "username": "Inventory Hunter",
+            "avatar_url": "https://styles.redditmedia.com/t5_2th52/styles/communityIcon_4411rfa4elr41.png?width=256&s=bba3f4384cbcb8590f768f4446d98f7b2017beb0",
         }
         try:
             logging.debug(f"Discord Webhook URL: {self.webhook_url}")
