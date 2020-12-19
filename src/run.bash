c13c0c4962a18702f46c83e321a7aa07e0fa422e
Eric Marti <EricJMarti@users.noreply.github.com>
2020-12-18 22:37:51 -0500

    Adding Xvfb

--- src/run.bash
+++ src/run.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export DISPLAY=:0
+
+Xvfb $DISPLAY -screen 0 1920x1080x24+32 &
+
 # If this looks stupid, that's because it is.
 # The issue is that selenium and/or chromedriver leave around zombie procs.
 # However, this issue magically goes away if bash owns the python process.
