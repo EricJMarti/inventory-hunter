13a44a745d3ba9bddda6a363cbc9f44ff99705a5
Eric Marti <EricJMarti@users.noreply.github.com>
2020-12-19 13:37:45 -0500

    Fixing PS Direct scraper

--- src/scraper/playstation.py
+++ src/scraper/playstation.py
@@ -16,21 +16,28 @@ class PlayStationScrapeResult(ScrapeResult):
             return
 
         # get name of product
-        tag = product.find('h2')
+        tag = product.find('h1')
+        if not tag:
+            tag = product.find('h2')
         if tag:
             alert_content += tag.text.strip() + '\n'
         else:
             self.logger.warning(f'missing title: {self.url}')
 
         # get listed price
-        tag = product.select_one('div.price-text > span.product-price')
-        price_str = self.set_price(tag)
-        if price_str:
-            alert_subject = f'In Stock for {price_str}'
+        tag = product.find('div', class_='price-text')
+        if tag:
+            price_symbol = tag.find('sup', class_='js-actual-price-symbol')
+            price_whole = tag.find('span', class_='js-actual-price-whole')
+            price_fraction = tag.find('sup', class_='js-actual-price-fraction')
+            if price_symbol and price_whole and price_fraction:
+                price_str = self.set_price(f'{price_symbol.text.strip()}{price_whole.text.strip()}.{price_fraction.text.strip()}')
+                if price_str:
+                    alert_subject = f'In Stock for {price_str}'
 
         # check for add to cart button
         tag = product.select_one('div.button-placeholder > button.add-to-cart')
-        if tag and 'add to cart' in tag.text.lower():
+        if tag and 'add' in tag.text.lower() and 'hide' not in tag.attrs['class']:
             self.alert_subject = alert_subject
             self.alert_content = f'{alert_content.strip()}\n{self.url}'
 
