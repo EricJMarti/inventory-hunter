e96781b099702f3cc4a8019a96f919a9fd83a470
titoubravin <80425517+titoubravin@users.noreply.github.com>
2021-03-11 00:06:44 -0500

    Update docker_run.bash

--- docker_run.bash
+++ docker_run.bash
@@ -17,7 +17,7 @@ usage() {
 [ $# -eq 0 ] && usage
 
 alerter="email"
-default_image="ericjmarti/inventory-hunter:latest"
+default_image="titoubravin/inventory-hunter:latest"
 image=$default_image
 
 while getopts a:c:d:e:i:q:r:w:t arg
